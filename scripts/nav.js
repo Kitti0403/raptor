9ef398f03876a5a368c96b50754ee9c6835b2af8
Csilluslol <106381397+Csilluslol@users.noreply.github.com>
2024-12-06 16:28:53 +0100

    nav - footer
    nem tudom hogy mukodik-e normalisan

---
+++ scripts/nav.js
@@ -0,0 +1,28 @@
+document.getElementById("navbar").innerHTML =
+` 
+    <div class="header">
+        <div class="nav">
+            <a href="../pages/index.html">
+                <div class="logo">
+                    <img alt="logo" src="images/noe_logo_no-bg.png">
+                </div>
+            </a>
+
+             <a href="../pages/timeline.html"><button>
+                Timeline
+            </button></a>
+
+            <a href="../pages/jurassic.html"><button> 
+                Jurassic Park
+            </button></a>
+
+        </div>
+    </div>
+`;
+
+document.getElementById("footer").innerHTML =
+`
+<footer id="footer">
+    <p>Annus Kitti  -  Doba Sándor  -  Muzsik Csilla</p>
+</footer>
+`
