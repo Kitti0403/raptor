c1a9e6e791862e985994f9bdd1626c00bc918900
Csilluslol <106381397+Csilluslol@users.noreply.github.com>
2024-12-08 22:03:58 +0100

    index nav

---
+++ scripts/in_nav.js
@@ -0,0 +1,26 @@
+document.getElementById("in_nav").innerHTML =
+` 
+    <div class="header">
+        <div class="nav">
+            <a class="logo" href="index.html">
+                <div>
+                    <img id="logo_img" alt="logo" src="images/rawr.png">
+                </div>
+            </a>
+
+             <a id="timeline" href="pages/timeline.html"><button class="btn">
+                Timeline
+            </button></a>
+
+            <a id="jurassic" href="pages/jurassic.html"><button class="btn"> 
+                Jurassic Park
+            </button></a>
+
+            <div id="clock"></div>
+        </div>
+    </div>
+`;
+document.getElementById("footer").innerHTML =
+`
+    <p>Annus Kitti  -  Doba Sándor  -  Muzsik Csilla</p>
+`
